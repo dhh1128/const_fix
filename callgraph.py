799c9ed2ea11acb93aa67f5d3aed9440d85b991f
Daniel Hardman <dhardman@adaptivecomputing.com>
2013-08-07 17:55:10 -0600

    progress

--- callgraph.py
+++ callgraph.py
@@ -1,11 +1,14 @@
 import os, re, sys
 
+doxy_output_folder = '/tmp/html'
+doxy_log = '/tmp/doxy-stdout'
+
 proto_pat = re.compile('<td class="memname">(.*?)</td>(.*?)Here is the call(er)? graph', re.DOTALL)
 references_pat = re.compile('<p>References(.*?)</p>', re.DOTALL)
 referenced_by_pat = re.compile('<p>Referenced by(.*?)</p>', re.DOTALL)
 link_pat = re.compile('">([^<]+\(\))</a>')
 
-def analyze(fname, by_caller, by_callee):
+def _analyze(fname, by_caller, by_callee):
     f = open(fname, 'r')
     try:
         txt = f.read()        
@@ -13,7 +16,7 @@ def analyze(fname, by_caller, by_callee):
         f.close()
     i = txt.find('<h2 class="groupheader">Function Documentation</h2>')
     if i > -1:
-        print(fname)
+        #print(fname)
         txt = txt[i:]
         for proto_match in proto_pat.finditer(txt):
             funcname = proto_match.group(1).strip()
@@ -43,37 +46,82 @@ def analyze(fname, by_caller, by_callee):
                     if caller not in x:
                         x.append(caller)
                         caller_count += 1
-            print('  %s: calls %d, called by %d' % (funcname, called_count, caller_count))
+            #print('  %s: calls %d, called by %d' % (funcname, called_count, caller_count))
         return True 
         
-def build_call_graphs(folder):
-    by_caller = {}
-    by_callee = {}
-    files = [f for f in os.listdir(folder) if f.endswith('.html')]
-    for item in files:
-        analyze(os.path.join(folder, item), by_caller, by_callee)
-    for caller in by_caller:
-        by_caller[caller].sort()
-    for callee in by_callee:
-        by_callee[callee].sort()
-    
-    orphans = []
-    for func in by_callee:
-        if not by_callee[func]:
-            orphans.append(func)
-            
-    leaves = []
-    for func in by_caller:
-        if not by_caller[func]:
-            leaves.append(func)
-            
-    print('\norphans\n-------')
-    for o in orphans:
-        print ('  %s' % o)
+def _call_doxygen(folder):
+    oldcwd = os.getcwd()
+    try:
+        os.chdir(folder)
+        exitcode = os.system('doxygen docs/Doxyfile >%s' % doxy_log)
+        return exitcode
+    finally:
+        os.chdir(oldcwd)
         
-    print('\nleaves\n-------')
-    for l in leaves:
-        print ('  %s' % l)
-    
-if __name__ == '__main__':
-    build_call_graphs(sys.argv[1])
+def _get_doxy_date(folder):
+    info = os.stat(os.path.join(folder, 'index.html'))
+    if info:
+        return info.st_mtime
+    return 0
+
+def _get_vcs_date(folder):
+    info = os.stat(os.path.join(folder, '.hg', 'branch'))
+    if info:
+        return info.st_mtime
+    return 0
+
+class Callgraph:
+    def __init__(self, root):
+        self.root = os.path.normpath(os.path.abspath(root))
+    def load(self):
+        doxy_lastmod = _get_doxy_date(doxy_output_folder)
+        vcs_lastmod = _get_vcs_date(self.root)
+        if doxy_lastmod < vcs_lastmod:
+            print('Re-running doxygen. Tail %s to monitor...' % doxy_log)
+            error = _call_doxygen(self.root)
+            if error:
+                sys.stderr.write('Doxygen failed with error code %d.\n', error)
+                sys.exit(1)
+        self._build_call_graphs()
+    def _build_call_graphs(self):
+        self.by_caller = {}
+        self.by_callee = {}
+        files = [f for f in os.listdir(doxy_output_folder) if f.endswith('.html')]
+        for item in files:
+            analyze(os.path.join(folder, item), by_caller, by_callee)
+        for caller in by_caller:
+            by_caller[caller].sort()
+        for callee in by_callee:
+            by_callee[callee].sort()
+        self._break_simple_recursion()
+    def get_orphans(self):
+        orphans = []
+        for func in self.by_callee:
+            if not self.by_callee[func]:
+                orphans.append(func)
+    def get_leaves(self):
+        leaves = []
+        for func in self.by_caller:
+            if not self.by_caller[func]:
+                leaves.append(func)
+    def remove(self, func):
+        callers = self.by_callee[func]
+        del self.by_callee[func]
+        for caller in callers:
+            self.by_caller[caller].remove(func)     
+    def is_empty(self):
+        return not (self.by_callee or self.by_caller)
+    def _break_simple_recursion(self):
+        '''
+        This only breaks recursion where a function calls itself directly;
+        indirect recursion is not detected.
+        '''
+        recursive = []
+        for func in self.by_caller:
+            called = self.by_caller[func]
+            if func in called:
+                recursive.append[func]
+                called.remove(func)
+                x = self.by_callee[func]
+                x.remove(func)
+        self.recursive = recursive
--- const_fix.py
+++ const_fix.py
@@ -1,11 +1,15 @@
 import os, sys, re, weakref
 import callgraph
 
+compile_cmd = 'make -j6 >/tmp/make.log'
+test_cmd = 'make -j6 test >/tmp/test.log'
 comment_pat = re.compile(r'/\*.*?\*/')
 datatype_names = 'int|short|long|double|float|char|bool'.split('|')
 moab_type_pat = re.compile('.*\Wm[a-z_0-9]+_t$')
 const_prefix_pat = re.compile('^const ([a-zA-Z0-9_]+)(.*)$')
 const_suffix_pat = re.compile('([a-zA-Z0-9_]+)\s+const.*')
+array_spec_pat = re.compile('.*(\[^]]\])$')
+prototype_pat_template = r'^\s*((?:[_a-zA-Z][_a-zA-Z0-9:]*)[^-()=+!<>/|^]*?(?:\s+|\*|\?))(%s)\s*\(([^()]*?)\)(\s*const)?\s*([{;])'
 
 def squeeze(txt):
     '''Replace all runs of whitepace with a single space, and trim front and back.'''
@@ -27,34 +31,35 @@ def normalize_type(typ):
         typ = '%s const%s' % (m.group(1), m.group(2))
     return typ
 
-def split_args(arglist):
+def split_params(param_list):
     '''
     Given a block of text that contains the parameter list for a func,
-    make a list of args out of it.'''
-    args = []
+    make a list of params out of it.'''
+    params = []
     paren_count = 0
     start = 0
-    arglist = arglist.strip()
-    if arglist:
-        for i in xrange(len(arglist)):
-            c = arglist[i]
+    param_list = param_list.strip()
+    if param_list:
+        for i in xrange(len(param_list)):
+            c = param_list[i]
             if c == ',' and paren_count == 0:
-                arg = squeeze(arglist[start:i])
-                assert(arg)
-                args.append(arg)
+                param = squeeze(param_list[start:i])
+                assert(param)
+                params.append(param)
                 start = i + 1
             elif c == '(':
                 paren_count += 1
             elif c == ')':
                 paren_count -= 1
-        arg = squeeze(arglist[start:])
-        assert(arg)
-        args.append(arg)
-    return args
+        param = squeeze(param_list[start:])
+        assert(param)
+        params.append(param)
+        params = [Param(p) for p in params]
+    return params
     
 def cut_cpp_comments(txt):
     '''remove c++-style comments from a block of text'''
-    args = arglist.split('\n')
+    args = param_list.split('\n')
     for n in xrange(len(args)):
         arg = args[n]
         k = arg.find('//')
@@ -75,6 +80,39 @@ def find_end_of_body(txt, first_body_idx):
             if curly_count == 0:
                 return i
     sys.stderr.write('Could not find end of function.')
+    
+class Param:
+    def __init__(self, decl):
+        self.decl = decl
+        self.array_spec = None
+        self.data_type = None
+        self.name = None
+        self._parse(decl)
+    def is_const_candidate(self):
+        return self.data_type.find['*'] > -1 or self.data_type.find['&'] > -1
+    def is_const(self):
+        return 
+    def _parse(self):
+        decl = squeeze(self.decl)
+        m = array_spec_pat.match(decl)
+        if m:
+            self.array_spec = m.group(1).replace(' ', '')
+            decl = decl[0:m.start(1)]
+        name_idx = -1
+        if not (decl.endswith('*') or decl.endswith('&') or moab_type_pat.match(decl)):
+            i = decl.rfind(' ')
+            if i > -1:
+                name_idx = i + 1
+                while decl[name_idx] == '*' or decl[name_idx] == '&':
+                    name_idx += 1
+                if name in datatype_names:
+                    name_idx = -1
+        if name_idx > -1:
+            self.data_type = decl[0:name_idx].rstrip()
+            self.name = decl[name_idx:]
+        else:
+            self.data_type = decl
+        self.data_type = normalize_type(self.data_type)
 
 class Prototype:
     def __init__(self, fpath, txt, match):
@@ -83,53 +121,104 @@ class Prototype:
         self.match = match
         self.original = self.txt[self.match.start():self.match.end()]
         self.body = None
-        if match.group(2) == '{':
-            self.body = find_end_of_body(txt, match.end(2))
-        # Find boundaries of arg list
+        self.return_type = match.group(1)
+        if match.group(4) == '{':
+            self.body = find_end_of_body(txt, match.end(4))
+        # Find boundaries of param list
         i = txt.find('(', match.end(1)) + 1
         j = txt.rfind(')', match.end())
-        arglist = txt[i:j]
+        param_list = txt[i:j]
         # Remove comments that might confuse us
-        arglist = comment_pat.sub('', arglist)
-        arglist = cut_cpp_comments(arglist)
+        param_list = comment_pat.sub('', param_list)
+        param_list = cut_cpp_comments(param_list)
         # Now split what's left. We can't just split on commas in
         # a simplistic way, since some args might be
         # function prototypes themselves, containing embedded commas
         # of their own...
-        self.args = split_args(arglist)
+        self.params = split_params(param_list)
+    def get_ideal(self):
+        return self.
+    def is_in_tests(self):
+        return 'test/' in self.fpath
+    def is_in_header(self):
+        return self.fpath.endswith('.h')
+    def is_in_impl(self):
+        return self.fpath.endswith('.c')
+    def is_const_candidate(self):
+        if self.params:
+            for p in self.params:
+                if p.is_const_candidate():
+                    return True
+        return False
     def matches(self, other):
         if len(self.args) == len(other.args):
             for i in xrange(len(self.args)):
                 type_a = self.get_arg_type(i)
                 type_b = self.
-    def arg_is_const_candidate(self, i):
-        arg = self.args[i]
-        return arg.find['*'] > -1 or arg.find['&'] > -1
-    def get_arg_type(self, i):
-        name = self.get_arg_name(i)
-        typ = self.args[i]
-        if name:
-            j = typ.rfind(name)
-            typ = typ[0:j]
-        return typ.strip()
-    def get_arg_name(self, i):
-        arg = self.args[i]
-        if arg.endswith('*') or arg.endswith('&') or moab_type_pat.match(arg):
-            return
-        j = arg.rfind(' ')
-        if j == -1:
-            return
-        name = arg[j+1:]
-        if name.startswith('*') or name.startswith('&'):
-            name = name[1:]
-        if name in datatype_names:
-            return
-        if name.endwith(']'):
-            name = name[0:name.rfind('[')]
-        return name
 
 def update_param_names(prototypes):
-    find_best_names_for_each_param(prototypes)
+    if len(prototypes) < 2:
+        return False
+    best = []
+    for i in xrange(len(prototypes)):
+        best[i] = ''
+    # Figure out which version of this parameter, across all prototypes,
+    # has the longest name. We're going to assume that the longest name
+    # is the best one. Not 100% true, I know, but a good approximation.
+    for proto in prototypes:
+        i = 0
+        for p in proto.params:
+            if p.name:
+                if best[i]:
+                    if len(best[i]) < len(p.name):
+                        best[i] = p.name
+                else:
+                    best[i] = p.name
+            i += 1
+    # Now see which prototypes need to be updated with better names.
+    change_count = 0
+    for proto in prototypes:
+        proto.dirty = False
+        i = 0
+        for p in proto.params:
+            if p.name != best[i]:
+                p.new_name = best[i]
+                proto.dirty = True
+            i += 1
+        if proto.dirty:
+            change_count += 1
+    # Now tweak all prototypes to use the best param names.
+    # We do it this way, instead of just generating a perfect
+    # prototype and inserting it where the old prototype used
+    # to be, so we can preserve unique comments, line spacing, and
+    # indents in each prototype.
+    if changed_count:
+        for proto in prototypes:
+            if proto.dirty:
+                bkpath, txt = backup_file(proto.fpath)
+                for param in proto.params:
+                    # TODO: this needs to be fixed. Won't work as-is, because our decls have
+                    # been squeezed and idealized already. Besides, this searches entire file
+                    # instead of the block of text where the parameters live.
+                    txt = re.sub(r'([^a-zA-Z0-9_])%s([^a-zA-Z0-9_])' % param.decl, param.ideal_version(), txt)
+                    # If we have a named param and a function body, update all usage of the name
+                    # inside the function.
+                    if param.name and proto.body:
+                        txt = re.sub(r'([^a-zA-Z0-9_])%s([^a-zA-Z0-9_])' % param.name, param.new_name, txt)
+                overwrite_file(proto.fpath, txt)
+    return changed_count > 0
+                
+def backup_file(fpath):
+    for i in xrange(1, 1000):
+        bkpath = fpath + '.' + str(i)
+        if not os.path.isfile(bkpath):
+            break
+    assert(bkpath < 1000)
+    with open(fpath, 'r') as f:
+        txt = f1.read()
+    with open(bkpath, 'w') as f:
+        f.write(txt)
+    return bkpath, txt
 
 def find_prototypes_in_file(func, fpath):
     with open(fpath, 'r') as f:
@@ -138,90 +227,127 @@ def find_prototypes_in_file(func, fpath):
     i = txt.find(func)
     if i == -1:
         return
-    expr = re.compile(START_LINE + RETURN_TYPE + '(' + func + ')' + ARGS + '(;|{)')
+    expr = re.compile(prototype_pat_template % func)
     protos = []
     for m in expr.finditer(txt):
         protos.append(Prototype(fpath, txt, m))
     return protos
 
-def find_prototypes_in_codebase(func, root):
+def find_prototypes_in_codebase(func, root, files=None):
     prototypes = {}
-    for root, dirs, files in os.walk(root):
-        skip = [d for d in dirs if d.startswith('.')]
-        for d in skip:
-            dirs.remove(d)
+    if files:
         for f in files:
-            if (not f.startswith('.')) and (f.endswith('.h') or f.endswith('.c')):
-                fpath = os.path.join(root, f)
-                in_this_file = find_prototypes_in_file(func, fpath)
-                if in_this_file:
-                    prototypes[fpath] = in_this_file
+            prototypes[fpath] = find_prototypes_in_file(f)
+    else:
+        for root, dirs, files in os.walk(root):
+            skip = [d for d in dirs if d.startswith('.')]
+            for d in skip:
+                dirs.remove(d)
+            for f in files:
+                if (not f.startswith('.')) and (f.endswith('.h') or f.endswith('.c')):
+                    fpath = os.path.join(root, f)
+                    in_this_file = find_prototypes_in_file(func, fpath)
+                    if in_this_file:
+                        prototypes[fpath] = in_this_file
     return prototypes
 
-def fix_func(func):
-    prototypes = get_prototypes(func)
+def tests_pass(root):
+    print('Testing...')
+    oldcwd = os.getcwd()
+    try:
+        os.chdir(root)
+        exitcode = os.system(compile_cmd)
+        if exitcode:
+            os.system('make clean > /tmp/x')
+            exitcode = os.system(compile_cmd)
+        return exitcode
+    finally:
+        os.chdir(oldcwd) 
+
+def compile_is_clean(root):
+    print('Compiling...')
+    oldcwd = os.getcwd()
+    try:
+        os.chdir(root)
+        exitcode = os.system(compile_cmd)
+        if exitcode:
+            os.system('make clean > /tmp/x')
+            exitcode = os.system(compile_cmd)
+        return exitcode
+    finally:
+        os.chdir(oldcwd) 
+
+def find_impl(prototypes):
+    if len(prototypes) == 1:
+        return prototypes[0]
+    for p in prototypes:
+        if p.body and p.is_in_impl() and not p.is_in_tests():
+            return p
+    for p in prototypes:
+        if p.body and not p.is_in_tests():
+            return p
+    for p in prototypes:
+        if not p.is_in_tests():
+            return p
+    return p[0]                
+
+def fix_func(func, root, cg):
+    # Locate every place where this function's prototype appears.
+    # In some cases, the prototype might be followed by a body; in most cases, not.
+    prototypes = find_prototypes_in_codebase(func, root)
+    # Change our code so function declarations display names for parameters.
+    # What we have in the code today, in headers, is often something like this:
+    #
+    #   int do_something(mjob_t *, char *, int, int, void *);
+    #
+    # This is an antipattern. You're supposed to be able to read a prototype and
+    # know how to use the function. So we're going to change the prototypes to
+    # look like this:
+    #
+    #   int do_something(mjob_t * job, char * buf, int size, int level, void * state);
     if update_param_names(prototypes):
         if not compile_is_clean():
             revert()
+        else:
+            # Reload prototypes from just the files that we modified.
+            x = {}
+            for p in prototypes:
+                x[p.fpath] = 1
+            prototypes = find_prototypes_in_codebase(func, root, x.keys())
+    # Find the version of the prototype that's associated with the main implementation
+    # of the function (not the one in scaffolding.c).
     impl = find_impl(prototypes)
-    if const_matters_to_func(impl):
+    if impl.is_const_candidate():
+        print('Checking %s.' % impl.get_ideal())
         for param in impl.params:
             recompile = False
-            if const_matters_to_param(param):
-                if is_const(param):
-                    recompile = True
-                    
-            elif is_const(param):
+            if param.is_const_candidate():
+                if not param.is_const():
+                    recompile = True                    
+            elif param.is_const():
                 recompile = True
                 remove_const(param)
             else:
                 recompile = False
                 
-class CallGraphNode:
-    def __init__(self, parent=None):
-        self.children = None
-        if parent:
-            self.parent = weakref.ref(parent)
-        else:
-            self.parent = None
-    def is_leaf(self):
-        return not self.children
-    def get_leaves(self):
-        leaves = []
-        if self.children:
-            for child in self.children:
-                if child.is_leaf():
-                    leaves.append(child)
-                else:
-                    leaves.extend(child.get_leaves())
-        return leaves
-    def remove_leaves(self):
-        if self.children:
-            to_remove = []
-            for child in self.children:
-                if child.is_leaf():
-                    to_remove.append(child)
-                else:
-                    child.remove_leaves()
-            for child in to_remove:
-                self.children.remove(child)
-            
 def fix_prototypes(root):
     root = os.path.normpath(os.path.abspath(root))
     if not os.path.isfile(os.path.join(root, 'Makefile')):
         sys.stderr.write('Folder %s is not the root of a codebase.\n' % root)
         return 1
-    if not code_compiles(root):
+    if not compile_is_clean(root):
         sys.stderr.write('Code in %s does not compile cleanly. Prototype fixup aborted.\n' % root)
         return 1
     if not tests_pass(root):
         sys.stderr.write('Code in %s does not compile cleanly. Prototype fixup aborted.\n' % root)
         return 1
-    funcs = load_call_graph(root)
-    while call_graph:
-        for func in call_graph.leaves():
-            fix_func(func)
-        call_graph.remove_leaves()
+    pass_number = 1
+    cg = callgraph.Callgraph(root)
+    while not cg.is_empty():
+        leaves = cg.get_leaves()
+        print('\nPass %d: %d leaves out of %d functions...\n' % pass_number, len(leaves), len(cg.by_callee))
+        for func in leaves:
+            fix_func(func, root, cg)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
